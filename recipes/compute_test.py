ed70440b3394c39ed2f3eaeef1b7f08f1dfff20e
admin <admin>
2022-06-24 18:21:40 +0100

    Recipe INCREMENTDATA.compute_test saved through notebook notebook editor for compute_test

--- recipes/compute_test.py
+++ recipes/compute_test.py
@@ -1,17 +1,48 @@
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # -*- coding: utf-8 -*-
 import dataiku
 import pandas as pd, numpy as np
 from dataiku import pandasutils as pdu
 
+# create an empty dataset with ID column 
 
+# dummy_data = pd.DataFrame(np.random.rand(100, 10))
+# dummy_data['Id'] = list(range(10))*10
 
-# Compute recipe outputs
-# TODO: Write here your actual code that computes the outputs
-# NB: DSS supports several kinds of APIs for reading and writing data. Please see doc.
+# # # save it to the dataframe 
+# inp = dataiku.Dataset("test")
+# inp.write_with_schema(dummy_data)
 
-test_df = ... # Compute a Pandas dataframe to write into test
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+# Read recipe inputs and take the input schema to output dataset 
 
+inp = dataiku.Dataset("test")
+out = dataiku.Dataset("output")
+out.write_schema_from_dataframe(inp.get_dataframe())
 
-# Write recipe outputs
-test = dataiku.Dataset("test")
-test.write_with_schema(test_df)
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+with out.get_writer() as writer:
+    for df in inp.iter_dataframes(chunksize=10):
+        
+        # Make some changes for the columns in the chunk 
+        tmp = df
+        tmp['Id'] = tmp['Id']+1000
+        tmp.iloc[0:10] = np.round(tmp.iloc[0:10] )
+        
+        print(tmp.head())
+        writer.write_dataframe(tmp)
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+# for some of the IDs we want to replace the content 
+# for i in range(5):
+
+# #    tmp = dummy_data[dummy_data['Id'] == i] * i
+#     tmp = np.round(dummy_data[dummy_data['Id'] == i] * (i+1000))
+
+#     with dataiku.Dataset("test").get_writer() as writer:
+
+#         for (origin,count) in tmp.items():
+#             print(count)
+
+#             writer.write_row_array((origin,count))
