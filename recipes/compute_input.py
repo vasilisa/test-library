f9f8ac2c597d9fc1f9beb44156c6665cc5c5bf48
admin <admin>
2022-06-24 18:50:57 +0100

    Saved recipe: INCREMENTDATA.compute_input

--- recipes/compute_input.py
+++ recipes/compute_input.py
@@ -4,6 +4,9 @@ import pandas as pd, numpy as np
 from dataiku import pandasutils as pdu
 
 
+#clear the content of Input and output datasets to start with 
+
+
 
 dummy_data = pd.DataFrame(np.random.rand(100, 10))
 dummy_data['Id'] = list(range(10))*10
